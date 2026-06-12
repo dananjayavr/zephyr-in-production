eb0b38e8a59ed098b033b03c14aee7fd7f39c1ce
dananjaya <>
2026-06-12 12:08:51 +0200

    test board FRDM-MCXN947

---
+++ FRDM-MCXN947.md
@@ -0,0 +1,10 @@
+Board info: https://docs.zephyrproject.org/latest/boards/nxp/frdm_mcxn947/doc/index.html
+
+Use this to flash + run: 
+
+west flash -r linkserver
+
+Example workflow: 
+
+west build -b frdm_mcxn947/mcxn947/cpu0 my_app --pristine
+west flash -r linkserver
---
+++ my_app/src/main.c
@@ -0,0 +1,36 @@
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+#define LED_NODE DT_ALIAS(led0)
+
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
+
+int main(void)
+{
+    LOG_INF("Starting my_app...\r\n");
+
+    bool led_state = true;
+
+    if (!gpio_is_ready_dt(&led)) {
+        return 0;
+    }
+
+    if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) {
+        return 0;
+    }
+
+    while (true) {
+        if (gpio_pin_toggle_dt(&led) < 0) {
+            return 0;
+        }
+
+        led_state = !led_state;
+        LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
+        k_msleep(1000);
+    }
+
+    return 0;
+}
